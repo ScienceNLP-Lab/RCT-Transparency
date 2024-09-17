0b01ca3a035df06a439c95aa8e35eca046391118
Mengfei Lan <39751535+MengfeiLan@users.noreply.github.com>
2024-09-16 23:19:09 -0500

    Add files via upload

--- inference/predict_onnx.py
+++ inference/predict_onnx.py
@@ -8,6 +8,7 @@ import nltk, requests
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import LancasterStemmer
+from sklearn.preprocessing import MultiLabelBinarizer
 
 import os, tqdm, datetime, re
 import pandas as pd
@@ -209,7 +210,7 @@ def merge(ml_ppredict, rule_based, article_level):
     return merged
 
 
-def main(model_path, tokenizer_name, csv_path, saved_path, column):
+def main_consort(model_path, tokenizer_name, csv_path, saved_path, column):
     sess_options = ort.SessionOptions()
     sess_options.intra_op_num_threads = 4
     sess_options.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL
@@ -290,6 +291,99 @@ def main(model_path, tokenizer_name, csv_path, saved_path, column):
     file.to_csv(saved_path, index=False)
     print("INFO: FILE SAVED")
 
+def main_spirit(model_path, tokenizer_name, csv_path, saved_path, column, mapping):
+    sess_options = ort.SessionOptions()
+    sess_options.intra_op_num_threads = 4
+    sess_options.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL
+    sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+
+    ort_sess = ort.InferenceSession(model_path, sess_options, providers=['CPUExecutionProvider'])
+
+    sentences, file = contextual_load(csv_path, tokenizer_name)
+    print("INFO: DATA LOADED")
+
+    # for 1a, 1b, 3b, 6b
+    rule_based_prediction = []
+    for i, row in file.iterrows():
+        pred_instance = []
+        if check_content(row['sentence'], phrases_3b):
+            pred_instance.append('3b')
+        if check_content(row['sentence'], phrases_6b):
+            pred_instance.append('6b')
+        rule_based_prediction.append([row['PMCID'], row['sentence_id'], pred_instance])
+    rule_based_prediction = pd.DataFrame(rule_based_prediction, columns=['PMCID', 'sentence_id', '3b_6b'])
+
+    # for 1a and 1b
+    article_prediction = []
+    abs_stru = structure()
+    for pmcid, article in file.groupby('PMCID'):
+        pred_a, pred_b = False, False
+        title_flag = article['section'].apply(check_title)
+        for i, row in article[title_flag].iterrows():
+            if not pred_a:
+                pred_a = check_1a(row['sentence'])
+                if pred_a:
+                    article_prediction.append([pmcid, row['sentence_id'], '1a'])
+            else:
+                break
+        abstract_flag = article["section"].apply(find_1b_candidates)
+        for i, row in article[abstract_flag].head(2).iterrows():
+            if not pred_b:
+                pred_b = check_1b_by_map(row['sentence'], abs_stru)
+                if pred_b:
+                    article_prediction.append([pmcid, row['sentence_id'], '1b'])
+            else:
+                break
+    article_prediction = pd.DataFrame(article_prediction, columns=['PMCID', 'sentence_id', '1a_1b'])
+    article_prediction = article_prediction.groupby(['PMCID', 'sentence_id']).agg(lambda x: x.tolist()).reset_index()
+
+    # bert predictions
+    batch_num = len(sentences) // 4 + (len(sentences) % 4)
+    all_result = []
+    progress = tqdm.tqdm(total=batch_num, ncols=75)
+
+    for batch in DataLoader(sentences, batch_size=4, shuffle=False,
+                            collate_fn=colloate_fn):
+        result = ort_sess.run(None,
+                               {
+                                   'text_ids': np.array(batch.text_ids, dtype='int32'),
+                                   'attention_mask_text': np.array(batch.attention_mask_text, dtype='float32')
+                               })
+        result = np.where(result[0] > 0.5, 1, 0)
+        all_result.extend(result.tolist())
+        progress.update(1)
+    progress.close()
+
+    labels = []
+    for inst in all_result:
+        label = []
+        for j in range(len(inst)):
+            if inst[j] == 1:
+                if column[j] not in ['3b', '6b']:
+                    label.append(column[j])
+        if len(label) == 0:
+            label.append('0')
+        labels.append(label)
+    file['label'] = labels
+    file = file.merge(rule_based_prediction, on=['PMCID', 'sentence_id'], how='outer')
+    file = file.merge(article_prediction, on=['PMCID', 'sentence_id'], how='outer')
+    file['label'] = file.apply(lambda x: merge(x['label'], x['3b_6b'], x['1a_1b']), axis=1)
+    file = file.drop(columns=['3b_6b', '1a_1b'])
+
+    convert_dict = dict()
+    for i, row in mapping[['SPIRIT', 'CONSORT']].dropna().iterrows():
+        convert_dict[row.tolist()[1]] = row.tolist()[0]
+
+    def convert(row):
+        converted = []
+        for item in row:
+            if item in convert_dict.keys():
+                converted.append(convert_dict[item])
+        return list(set(converted))
+
+    file["label"] = file['label'].apply(lambda x: convert(x))
+    file.to_csv(saved_path, index=False)
+    print("INFO: FILE SAVED")
 
 def get_elapsed_time(start_time, end_time):
 
@@ -321,9 +415,19 @@ if __name__ == '__main__':
     # path to the file to be saved
     saved_path = 'prediction_covid.csv'
     start_time = datetime.datetime.now()
+    guideline = "consort"
+    # choose guideline from "consort" and "spirit"
     column = ['10', '11a', '11b', '12a', '12b', '13a', '13b', '14a', '14b', '15', '16', '17a', '17b',
               '18', '19', '20', '21', '22', '23', '24', '25', '2b', '3a', '3b', '4a', '4b', '5',
               '6a', '6b', '7a', '7b', '8a', '8b', '9']
-    main(model_path, tokenizer_name, folder_path, saved_path, column)
+
+    if guideline == "consort":
+        main_consort(model_path, tokenizer_name, folder_path, saved_path, column)
+
+    elif guideline == "spirit":
+        mapping = pd.read_csv('mapping.csv', header=0)
+        main_spirit(model_path, tokenizer_name, folder_path, saved_path, column, mapping)
+
+
     end_time = datetime.datetime.now()
     print("INFO: Time spent: ", get_elapsed_time(start_time, end_time))
